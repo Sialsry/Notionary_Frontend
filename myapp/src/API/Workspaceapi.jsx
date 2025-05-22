385734f550645ee417ffc998f7685e3eb77b2632
blackorion <104806177+Mr-Binod@users.noreply.github.com>
2025-05-23 01:28:09 +0900

    check

--- myapp/src/API/Workspaceapi.jsx
+++ myapp/src/API/Workspaceapi.jsx
@@ -4,9 +4,11 @@ import axios from "axios";
 const WORKSPACE_URL = 'http://localhost:4000';
 
 
-    const saveData = async ({data}) => {
+    const saveData = async (_data) => {
+        console.log('check', typeof (_data))
         try {
-            const { data } = await axios.post(`${WORKSPACE_URL/'saveData'}`, {data})
+            const { data } = await axios.post(`${WORKSPACE_URL}/saveData`, {_data})
+            console.log(data,'axios')
             return {state : 200, message : 'success'}
         } catch (error) {
             return {state : 403, message : error}
